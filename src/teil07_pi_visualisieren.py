48ec34d0017e3984c99f43fa8b34ab9645a285c2
kernbeisser <kernbeisser@tom-bonn.de>
2022-05-02 13:43:37 +0200

    teil 07 pi visualisieren

---
+++ src/teil07_pi_visualisieren.py
@@ -0,0 +1,39 @@
+# imports as needed
+import turtle as tu
+import random
+
+def read_pi():
+    with open("./data/Teil_04_1_million_digits_of_pi.txt") as f:
+        pi = f.read()
+    return pi
+
+def visualize_pi(pi):
+    lines = 10000
+
+    tu.speed(0)
+    tu.tracer(False)
+    tu.screensize(1800, 1200, 'black')
+    tu.colormode(255)
+
+    for n in range(lines):
+        zahl = pi[n]
+        rotation = int(zahl) * 36
+        tu.setheading(rotation)
+
+        r = random.randint(45, 255)
+        g = random.randint(145, 255)
+        b = random.randint(45, 255)
+        tu.pencolor((r, g, b))
+
+        tu.forward(11)
+
+    tu.done()
+
+def main():
+    pi = read_pi()
+    # print(f"{pi[:10]}")
+
+    visualize_pi(pi)
+
+if __name__ == '__main__':
+    main()
