5914efc39e77e447e1c1fbada2ed815f2cff7aba
kernbeisser <kernbeisser@tom-bonn.de>
2022-05-08 07:38:18 +0200

    rename class template for ordering; teil14 g_ranking

---
+++ src/teil14_g_ranking.py
@@ -0,0 +1,18 @@
+# import random
+
+
+class ClassName:
+    def __init__(self):
+        self.width = 1024
+        self.height = 1024
+
+    def setup(self):
+        print("much ado about nothing ...")
+
+
+def main():
+    cn = ClassName()
+    cn.setup()
+
+if __name__ == "__main__":
+    main()
